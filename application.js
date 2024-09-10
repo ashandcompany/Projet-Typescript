fc85074a43a3b3d48f65bed335d06869ae88ae04
Emma Roizot <emma.roizot@orange.fr>
2024-09-10 15:36:57 +0200

    Petit programme de puissance

--- application.js
+++ application.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+"use strict";
+function puissance(a, b) {
+    let result = 1;
+    for (let i = 0; i < b; i++) {
+        result = result * a;
+    }
+    console.log(result);
+    return result;
+}
+let calcul = 1 / puissance(2, 3);
+console.log(calcul);
--- application.ts
+++ application.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function puissance(a : number, b : number){
+
+    let result = 1;
+
+    for (let i = 0; i < b; i++) {
+        result = result * a;
+    }
+
+    console.log(result);
+    return result
+}
+
+let calcul = 1 / puissance(2, 3);
+console.log(calcul);
