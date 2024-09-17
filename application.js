5ff3774af7ab3206f37031083124ba705d9309a7
Emma Roizot <emma.roizot@orange.fr>
2024-09-17 13:15:35 +0200

    Corrections

--- application.js
+++ application.js
@@ -1,11 +1,31 @@
 "use strict";
-function puissance(a, b) {
-    let result = 1;
-    for (let i = 0; i < b; i++) {
-        result = result * a;
+class Point {
+    constructor(abs, ord) {
+        this.abs = abs;
+        this.ord = ord;
     }
-    console.log(result);
-    return result;
+    getAbs() {
+        return this.abs;
+    }
+    getOrd() {
+        return this.ord;
+    }
+    sePresenter() {
+        console.log(`Mon abscisse est ${this.abs} et mon ordonnée ${this.ord}`);
+    }
+    calculerDistance(point) {
+        let distance = Math.sqrt(Math.pow(point.abs - this.abs, 2) + Math.pow(point.ord - this.ord, 2));
+        return distance;
+    }
+    calculerMilieu(p) {
+        let x = (p.abs + this.abs) / 2;
+        let y = (p.ord + this.ord) / 2;
+        let z = new Point(x, y);
+        return z;
+    }
 }
-let calcul = 1 / puissance(2, 3);
-console.log(calcul);
+let a = new Point(2, 3);
+a.sePresenter();
+let b = new Point(5, 7);
+console.log(`Distance entre les deux points: ${a.calculerDistance(b)}`);
+console.log(`Coordonnées du milieu : (${a.calculerMilieu(b).abs},${a.calculerMilieu(b).ord})`);
