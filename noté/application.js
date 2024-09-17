765f353ce7101ea96d89752a7b8d7361ce3ca26e
Emma Roizot <emma.roizot@orange.fr>
2024-09-17 15:05:29 +0200

    Première partie du tp noté

---
+++ noté/application.js
@@ -0,0 +1,30 @@
+"use strict";
+class Pokemon {
+    constructor(nom, pointsDeVie, pointsAttaque) {
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.pointsAttaque = pointsAttaque;
+    }
+    get_nom() {
+        return this.nom;
+    }
+    set_pointsDeVie(pdv) {
+        this.pointsDeVie = pdv;
+    }
+    afficher_informations() {
+        console.log(`Pokemon : ${this.nom}. HP restants : ${this.pointsDeVie} HP. Attaque : ${this.pointsAttaque}.`);
+    }
+    estMort() {
+        return (this.pointsDeVie <= 0);
+    }
+    attaquer(p) {
+        console.log(`Oh non ! ${p.get_nom()} a perdu ${this.pointsAttaque} HP !`);
+        p.set_pointsDeVie(p.pointsDeVie - this.pointsAttaque);
+    }
+}
+let pikachu = new Pokemon("Pikachu", 10, 6);
+let bulbizarre = new Pokemon("Bulbizarre", 23, 2);
+console.log(pikachu.estMort());
+pikachu.afficher_informations();
+bulbizarre.attaquer(pikachu);
+pikachu.afficher_informations();
